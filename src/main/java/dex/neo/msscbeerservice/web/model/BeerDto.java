f56b7d6b099ff53d9123bb4647e50486f39e4018
Malay <malayletsrock@gmail.com>
2021-04-13 11:51:47 +0100

    adding web DTOs closes #2

---
+++ src/main/java/dex/neo/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,26 @@
+package dex.neo.msscbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+    private BeerStyleEnum beerStyle;
+    private Long upc;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+
+}
---
+++ src/main/java/dex/neo/msscbeerservice/web/model/BeerPageList.java
@@ -0,0 +1,20 @@
+package dex.neo.msscbeerservice.web.model;
+
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+/**
+ * Created by Malay Shukla
+ */
+public class BeerPageList extends PageImpl<BeerDto> {
+
+    public BeerPageList(List<BeerDto> content, Pageable pageable, long total) {
+        super(content, pageable, total);
+    }
+
+    public BeerPageList(List<BeerDto> content) {
+        super(content);
+    }
+}
